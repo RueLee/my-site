d7c148c175180fde6362c24d9887dfc6213c6208
RueLee <ruelee125@gmail.com>
2025-12-03 01:06:45 -0800

    Error check

--- src/backend/mysite/settings.py
+++ src/backend/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG') == True
 
-ALLOWED_HOSTS = ['.awsapprunner.com']
+ALLOWED_HOSTS = ['.awsapprunner.com', '.ruleonlee.com']
 
 
 # Application definition
