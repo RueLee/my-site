eb2d8b26077c180886ecf21e9b724720fe4ebdb3
ruelee <ruelee125@gmail.com>
2026-03-01 04:27:35 -0800

    Revert "Moved programming language from TS to JS"
    This reverts commit 933718ebb5c1cb2af7573397ba28ca56b855fee6.

--- src/frontend/app/layout.tsx
+++ src/frontend/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import Image from "next/image"
 import Link from "next/link"
 import { inter, lemonmilk } from "./components/fonts"
@@ -12,12 +13,16 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Ruleon Ki Lee",
   description: "Current UC Irvine Student and Computer Science Enthusiast",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en" suppressHydrationWarning className={inter.className}>
       <body className="flex flex-col min-h-screen">
--- src/frontend/lib/api.ts
+++ src/frontend/lib/api.ts
@@ -1,6 +1,6 @@
-const API_ROOT_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_ROOT_URL = process.env.NEXT_PUBLIC_API_BASE_URL!;
 
-export async function getApiPost(slug) {
+export async function getApiPost(slug: string) {
     const res = await fetch(`${API_ROOT_URL}blogposts/${slug}`);
     if (!res.ok) { throw new Error(`Unable to load API! Error Code: ${res.status}`); }
     return res.json();
