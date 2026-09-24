cea88613122b246fad3abde26bc20830a1764b9b
RueLee <ruelee125@gmail.com>
2026-09-23 18:10:24 -0700

    feat: remove backend structure and adjusted .gitignore

--- src/backend/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import BlogPost
-
-class BlogPostSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
-    class Meta:
-        model = BlogPost
-        fields = "__all__"
